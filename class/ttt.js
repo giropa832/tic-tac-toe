3458ee95f74e9b0398da3b376d25302bf5d64ae9
Jerónimo Palacios <giropa832@gmail.com>
2022-12-14 21:42:45 +0100

    Implemented winning logic

--- class/ttt.js
+++ class/ttt.js
@@ -30,10 +30,49 @@ class TTT {
 
   static checkWin(grid) {
 
-    // Return 'X' if player X wins
-    // Return 'O' if player O wins
-    // Return 'T' if the game is a tie
-    // Return false if the game has not ended
+    const horizontalWinX = ['X','X','X'];
+    const horizontalWinO = ['O','O','O'];
+
+   
+
+    for (let i = 0; i < grid.length; i++) {
+      if (grid[i].toString() == horizontalWinX.toString()) {
+        return 'X';
+      };
+      if (grid[i].toString() == horizontalWinO.toString()) {
+        return 'O';
+      };
+      if (grid[0][i] == 'X' && grid[1][i] == 'X' && grid[2][i] == 'X') {
+        return 'X';
+      };
+      if (grid[0][i] == 'O' && grid[1][i] == 'O' && grid[2][i] == 'O') {
+        return 'O';
+      };
+    };
+
+    if(grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') {
+      return 'X';
+    };
+
+    if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O') {
+      return 'O';
+    };
+
+    if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') {
+      return 'X';
+    }
+
+    if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O') {
+      return 'O';
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+      if(grid[0].includes(' ')) {
+        return false;
+      }
+    }
+
+    return 'T';
 
   }
 
